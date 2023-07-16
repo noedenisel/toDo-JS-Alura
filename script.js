15b1042540cc90d36e265f298b627f6979ddfb74
NoeDenise <noelialombardo@hotmail.com>
2023-07-16 20:39:22 -0300

    agregando task

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ const cerateTask = (evento) =>
     evento.preventDefault()
     console.log("crear tarea");
     const input = document.querySelector("[data-form-input]")
-    console.log(input.value);
+    const value = input.value  
+    console.log(input.value);  
+    const task = document.querySelector("[data-task]")
+    console.log(task);
+    const content = `   <div>
+                            <i class="far fa-check-square icon"></i>
+                            <span class="task">${value}</span>
+                        </div>
+                        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content
+                        console.log(content);
+    input.value = ""
+
 }
 
 //? tenemos el  btn y queremos un listener para cuando el usuario haga click
